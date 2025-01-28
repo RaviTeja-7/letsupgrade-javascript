3cc8aa801a40b7e71e3a12a46e9b674da370a073
Ravi Teja <lankeraviteja7@gmail.com>
2025-01-29 02:31:40 +0530

    updatee

---
+++ FoodMaze/src/main/java/com/fp/FoodMaze/FoodMazeApplication.java
@@ -0,0 +1,13 @@
+package com.fp.FoodMaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodMazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodMazeApplication.class, args);
+	}
+
+}
---
+++ FoodMaze/src/test/java/com/fp/FoodMaze/FoodMazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fp.FoodMaze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodMazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
